c3bc33647246978f9cefde865520d83c4f20f4c3
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2022-04-05 13:31:28 +0200

    Update APIforDiseaseGeneNet.py
    Updated get Disease

--- APIforDiseaseGeneNet.py
+++ APIforDiseaseGeneNet.py
@@ -38,19 +38,26 @@ class GetInformationFromDiseaseGeneNet:
             self.response_gene = response_gene
 
     def disease_information(self,disease_of_interest):
+        self.session.headers.update({"Authorization": "Bearer %s" % self.api_key})
         if disease_of_interest.__len__() != 1:
+            diseases_for_url=['%20'.join(disease) for disease in disease_of_interest]
             seperation_pattern = '%2C'
-            disease_pattern_in_url=seperation_pattern.join(disease_of_interest)
-            url_to_take=self.url+'disease/'+disease_pattern_in_url+'?source=CURATED'
-            response_disease = requests.get(url_to_take)
+            disease_pattern_in_url=seperation_pattern.join(diseases_for_url)
+            url_to_take=self.url+'/gda/disease/'+disease_pattern_in_url
+            response_disease = self.session.get(url_to_take,params={'source':'CURATED'})
         else:
-            url_to_take = self.url+'disease/'+disease_of_interest+'?source=CURATED'
-            response_disease = requests.get(url_to_take)
+            disease_for_url=['%20'.join(x.split()) for x in disease_of_interest]
+            seperation_pattern = '%2C'
+            disease_pattern_in_url=seperation_pattern.join(disease_for_url)
+            url_to_take = self.url+'/gda/disease/'+disease_pattern_in_url
+            print(url_to_take)
+            response_disease = self.session.get(url_to_take,params={'source':'CURATED'})
         self.response_disease = response_disease
 
-    def format_information_to_gene_and_disease_only(self):
+
+    def format_information_to_gene_and_disease_only(self,information_to_take):
         keys_of_interest = ('gene_symbol','disease_name')
-        dictionary_from_json = self.response_gene.json()
+        dictionary_from_json = information_to_take.json()
         dataframe_disease_gene = pd.DataFrame.from_dict(dictionary_from_json)
         dataframe_disease_gene = dataframe_disease_gene.loc[:,keys_of_interest]
         type_interaction_disease = pd.DataFrame({'Type':['Gene-Disease' for x in range(dataframe_disease_gene.shape[0])]})
